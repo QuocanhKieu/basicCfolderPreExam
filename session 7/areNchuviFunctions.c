590fd07ae367d3a868e2184fe036f1c5f3d4a441
QuocanhKieu <44871775+QuocanhKieu@users.noreply.github.com>
2023-05-30 08:44:17 +0700

    Add files via upload

--- session 7/areNchuviFunctions.c
+++ session 7/areNchuviFunctions.c
@@ -17,13 +17,35 @@ int dienTichHcn(int a, int b) {
 
 
 int main() {
-	int a, b;
+	int a, b, selection;
 	printf("Nhap vao 2 canh hcn: a , b : ");
 	scanf("%d %d", &a, &b );
 	
-	printf("Chu vi hcn = %d\n", chuViHcn(a, b) );
-	printf("Dien tich hcn = %d", dienTichHcn(a, b) );
-
+	
+	do {
+		printf("1, Nhap vao 1 de tinh chu vi\n");
+		printf("2, Nhap vao 2 de tinh dien tich\n");
+		printf("3, Nhap 3 de thoat \n\n");
+		
+		printf("Lua chon cua ban la: ");
+		scanf("%d", &selection);
+		
+		switch(selection) {	
+			case 1: 
+				printf("Chu vi hcn = %d\n", chuViHcn(a, b) );
+				break;
+			case 2: 
+				printf("Dien tich hcn = %d", dienTichHcn(a, b) );
+				break;
+			case 3:
+				printf("Hen gap lai !\n");
+				break;
+			default:
+				printf("Lua chon khong dung, vui long nhap lai\n");
+		}
+		printf("\n\n");
+	} while(selection != 3);
+	
 
 	return 0;
 }
